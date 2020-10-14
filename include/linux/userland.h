8c785990da318f4fbbae6cdfcbc722bac6102c16
tbalden <illespal@gmail.com>
2020-10-14 14:36:27 -0700

    userland: extend with selinux state calls

--- include/linux/userland.h
+++ include/linux/userland.h
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2020 Vlad Adumitroaie <celtare21@gmail.com>.
+ * Copyright (C) 2020 Vlad Adumitroaie <celtare21@gmail.com>
+ * 2020 Pal Zoltan Illes @tbalden at github
  */
 
 extern struct selinux_state *get_extern_state(void);
 extern struct selinux_state *extern_state;
 extern bool is_decrypted;
+
+extern int get_enforce_value(void);
+extern void set_selinux(int value);
