782b05b412f36d8bca4399a41146d98baadf133d
tbalden <illespal@gmail.com>
2020-10-14 14:34:45 -0700

    binaries: add systools for possible wifi ssid name grep

---
+++ binaries/systools.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+# use dumpsys to get netstats and grep for networId line on wlan, and get 3rd string -> networkId="xyz"
+dumpsys netstats | grep -E 'iface=wlan.*networkId'|head -1|awk -F, '{print $3}' > /data/local/tmp/cs-systools.txt 2> /data/local/tmp/cs-systools.err
+
