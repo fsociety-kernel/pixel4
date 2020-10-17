13e579807c2eef79f0dfedd94125d95765131a32
wrongway213 <tropeazy@gmail.com>
2020-10-17 09:11:37 -0400

    Revert "userland: Only fix TEE if not rooted"
    This reverts commit a244a47754a0daca76e6e0af65f925149502b4ae.

--- init/userland_worker.c
+++ init/userland_worker.c
@@ -209,8 +209,6 @@ static void fix_TEE(void)
 
 	msleep(SHORT_DELAY * 2);
 
-	pr_info("Fixing TEE");
-
 	strcpy(argv[0], "/data/local/tmp/resetprop_static");
 	strcpy(argv[1], "ro.product.model");
 	strcpy(argv[2], "Pixel 4a");
@@ -296,16 +294,7 @@ static void encrypted_work(void)
 		return;
 	}
 
-	strcpy(argv[0], "/system/bin/sh");
-	strcpy(argv[1], "-c");
-	strcpy(argv[2], "/system/bin/su");
-	argv[3] = NULL;
-
-	ret = use_userspace(argv);
-	if (ret)
-		fix_TEE();
-	else
-		pr_info("Root detected! Skipping TEE fix");
+	fix_TEE();
 
 	strcpy(argv[0], "/system/bin/setprop");
 	strcpy(argv[1], "pixel.oslo.allowed_override");
